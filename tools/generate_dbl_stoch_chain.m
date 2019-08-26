996cae9f8060cac10f9e188edd3a3bf8172d2d88
Lisa Li <flyingpeach@live.com>
2019-08-26 16:30:01 -0700

    Add new syntax to chain generators

--- tools/generate_dbl_stoch_chain.m
+++ tools/generate_dbl_stoch_chain.m
@@ -1,33 +1,32 @@
-function [A,B] = generate_dbl_stoch_chain(Nx,alpha,rho,act_dens);
-% Nx = size of chain
-% alpha is how much state is spread between neighbors
-% rho is stabiilty of A.  Choose rho = 1 for dbl-stoch A
-% act_dens \in (0,1] is atuation density.  This is approximate (only exact
-% if things divide into each other evenly)
+function generate_dbl_stoch_chain(sys, rho, actDens, alpha)
+% Populates (A, B2) of the specified system with these dynamics:
+
 % x_1(t+1) = rho*[(1-alpha)*x_1(t) + alpha x_2(t)] + B(1,1)u_1(t)
-% x_i(t+1) = rho*[alpha*x_{i-1}(t) + (1-2*alpha)x_i(t) + alpha*x_{i+1}(t)]
-% + B(i,i)u_i(t)
+% x_i(t+1) = rho*[alpha*x_{i-1}(t) + (1-2*alpha)x_i(t) + alpha*x_{i+1}(t)] + B(i,i)u_i(t)
 % x_N(t+1) = rho*[alpha*x_{N-1}(t) + (1-alpha)x_N(t)] + B(N,N)u_N(t)
 
+% Inputs
+%    sys     : an LTISystem
+%    rho     : stability of A; choose rho=1 for dbl stochastic A
+%    actDens : actuation density of B, in (0, 1]
+%              this is approximate; only exact if things divide exactly
 
-if (nargin < 1)
-    alpha = .2;
-    rho = 1;
-    Nx = 9;
-    act_dens = 1/3;
+if isempty(sys.Nx) % Expect this to be nonempty
+    error('Nx in the LTISystem is empty! Please specify it first')
 end
 
-Nu = ceil(Nx*act_dens);
+sys.Nu = ceil(sys.Nx * actDens);
 
-A = (1-2*alpha)*speye(Nx);
+A = (1-2*alpha)*speye(sys.Nx);
 A(1,1) = A(1,1) + alpha;
-A(Nx,Nx) = A(Nx,Nx) + alpha;
-A(1:end-1,2:end) = A(1:end-1,2:end)+ alpha*speye(Nx-1);
-A(2:end,1:end-1) = A(2:end,1:end-1)+ alpha*speye(Nx-1);
+A(sys.Nx,sys.Nx) = A(sys.Nx,sys.Nx) + alpha;
+A(1:end-1,2:end) = A(1:end-1,2:end)+ alpha*speye(sys.Nx-1);
+A(2:end,1:end-1) = A(2:end,1:end-1)+ alpha*speye(sys.Nx-1);
 
-A = A*rho;
-
-B = sparse(Nx, Nu);
-for i=1:1:Nu
-    B(mod(floor(1/act_dens*i-1),Nx)+1,i) = 1;
+B = sparse(sys.Nx, sys.Nu);
+for i=1:1:sys.Nu
+    B(mod(floor(1/actDens*i-1), sys.Nx)+1,i) = 1;
 end
+
+sys.A = A*rho;
+sys.B2 = B;
--- tools/generate_rand_chain.m
+++ tools/generate_rand_chain.m
@@ -1,17 +1,26 @@
-function [A,B] = generate_rand_chain(Nx,rho,act_dens);
-% Nx = size of chain
-% generates a random chain (tridiagonal A matrix) with random B matrix of
-% specified actuation density. Normlaized so max |eig(A)| = rho;
+function generate_rand_chain(sys, rho, actDens)
+% Populates (A, B2) of the specified system with a random chain 
+% (tridiagonal A matrix) and a random actuation (B) matrix
+% Inputs
+%    sys     : an LTISystem
+%    rho     : normalization value; A is generated s.t. max |eig(A)| = rho
+%    actDens : actuation density of B, in (0, 1]
+%              this is approximate; only exact if things divide exactly
 
-Nu = ceil(Nx*act_dens);
-
-A = speye(Nx);
-A(1:end-1,2:end) = A(1:end-1,2:end)+ diag(randn(Nx-1));
-A(2:end,1:end-1) = A(2:end,1:end-1)+ diag(randn(Nx-1));
-
-A = A/max(abs(eigs(A)))*rho;
-
-B = sparse(Nx, Nu);
-for i=1:1:Nu
-    B(mod(floor(1/act_dens*i-1),Nx)+1,i) = randn();
+if isempty(sys.Nx) % Expect this to be nonempty
+    error('Nx in the LTISystem is empty! Please specify it first')
 end
+
+sys.Nu = ceil(sys.Nx * actDens);
+
+A                = speye(sys.Nx);
+A(1:end-1,2:end) = A(1:end-1,2:end)+ diag(randn(sys.Nx-1));
+A(2:end,1:end-1) = A(2:end,1:end-1)+ diag(randn(sys.Nx-1));
+
+B = sparse(sys.Nx, sys.Nu);
+for i=1:1:sys.Nu
+    B(mod(floor(1/actDens*i-1), sys.Nx)+1,i) = randn();
+end
+
+sys.A  = A / max(abs(eigs(A))) * rho;
+sys.B2 = B;
