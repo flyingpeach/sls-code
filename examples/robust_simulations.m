7bd33e6e3c5a3c1af100c6a77e7e6506c7944a05
Lisa Li <flyingpeach@live.com>
2019-08-28 19:07:57 -0700

    Make robust_simulations compatible with new functions

--- examples/robust_simulations.m
+++ examples/robust_simulations.m
@@ -1,53 +1,65 @@
-clc; clear; close all;
+clear; close all; clc; 
 
-Nx = 50; % states
-loc = floor(Nx/2); % specify where disturbance hits
+% specify system matrices
+sys    = LTISystem;
+sys.Nx = 50;
 
-actuation = 0.5; % actuation density
-alpha = 0.4;
-rho = 1;
+alpha = 0.4; rho = 1; actDens = 0.5;
+generate_dbl_stoch_chain(sys, rho, actDens, alpha); % generate sys.A, sys.B2
 
-% Construct system matrices
-[A,B] = generate_dbl_stoch_chain(Nx,alpha,rho,actuation);
-[~,Nu] = size(B); % number of actuators
+sys.B1  = eye(sys.Nx); % used in simulation
+sys.C1  = [speye(sys.Nx); sparse(sys.Nu, sys.Nx)]; % used in H2/HInf ctrl
+sys.D12 = [sparse(sys.Nx, sys.Nu); speye(sys.Nu)];
 
-% Specify objective function parameters
-C = [speye(Nx); sparse(Nu,Nx)];
-D = [sparse(Nx,Nu); speye(Nu)];
+% sls parameters
+slsParams           = SLSParams;
+slsParams.obj_      = Objective.H2;
+slsParams.mode_     = SLSMode.ApproxDLocalized;
+slsParams.tFIR_     = 10;
+slsParams.actDelay_ = 1;
+slsParams.d_        = 6;
+slsParams.robCoeff_ = 1000;
 
-T = 10;
+% simulation parameters
+simParams           = SimParams;
+simParams.tSim_     = 25;
+simParams.w_        = zeros(sys.Nx, simParams.tSim_); % disturbance
+simParams.w_(floor(sys.Nx/2), 1) = 10;
+simParams.openLoop_ = false;
 
-% locality
-d = 6; 
-ta = 1;
+cSpeeds = [2 1.5 1.4 1.3 1.2 1.1 1 0.9 0.8 0.7 0.6 0.5 0.4];
+cSims   = [2 1 0.4]; % which comm speeds to simulate & plot
 
-Tmax = 25;
-Tsim = T + Tmax;
+clnorms     = zeros(length(cSpeeds), 1); 
+robustStabs = zeros(length(cSpeeds), 1);
 
-comms = [2 1.5 1.4 1.3 1.2 1.1 1 0.9 0.8 0.7 0.6 0.5 0.4];
-lambda = 1000;
-for i=1:length(comms)
-   [R2,M2,obj(i),robust_stab(i)]  = sf_sls_approx_d_localized(A,B,C,D,T,d,comms(i),ta,lambda,'H2');
-    make_heat_map(A,B,T,Nx,Nu,R2,M2,loc,Tsim,['Comms = ',num2str(comms(i))])
+for i=1:length(cSpeeds)
+    slsParams.cSpeed_ = cSpeeds(i);
+    slsOuts = state_fdbk_sls(sys, slsParams);
+
+    if ismember(cSpeeds(i), cSims)
+        [x, u] = simulate_system(sys, slsParams, slsOuts, simParams);
+        plot_heat_map(x, sys.B2*u, ['Comms = ',num2str(cSpeeds(i))]);
+    end
+
+    clnorms(i)     = slsOuts.clnorm_;
+    robustStabs(i) = slsOuts.robustStab_;    
 end
 
-figure
-p1 = plot(comms,obj,'o-');
-set ( gca, 'xdir', 'reverse' )
-title([int2str(Nx), ' Node Chain' ])
-xlabel('Comms Speed')
-ylabel('Localized H_2-Norm Cost')
-set(gca,'FontSize',16,'fontWeight','bold')
-set(p1,'Color','red')
-set(p1,'LineWidth', 2)
+figure;
+p1=plot(cSpeeds, clnorms,'o-');
+set(gca, 'xdir', 'reverse');
+title([int2str(sys.Nx), ' Node Chain' ]);
+xlabel('Comm Speed'); ylabel('Localized H_2-Norm Cost');
 
 figure
-p2 = plot(comms,robust_stab,'o-');
-set ( gca, 'xdir', 'reverse' )
-title([int2str(Nx), ' Node Chain' ])
-xlabel('Comms Speed')
-ylabel('Stability Margin')
-set(gca,'FontSize',16,'fontWeight','bold')
-set(p2,'Color','red')
-set(p2,'LineWidth', 2)
+p2=plot(cSpeeds,robustStabs,'o-');
+set(gca, 'xdir', 'reverse');
+title([int2str(sys.Nx), ' Node Chain' ]);
+xlabel('Comm Speed'); ylabel('Stability Margin');
+
+% These are the font settings from previous papers; uncomment as wanted
+% set(gca,'FontSize',16,'fontWeight','bold')
+% set(p1,'Color','red')
+% set(p1,'LineWidth', 2)
 
