4ef056bdee63629c8c83b10e9cbbdba94cd1c335
Lisa Li <flyingpeach@live.com>
2019-09-09 20:18:29 -0700

    Add trailing zero constraints
    Previous implementation was missing these constraints,
    so was an approximation; can specify "approx" in solver
    to use the previous implementation

--- code/lisa/alt_impl/find_alt_impl.m
+++ code/lisa/alt_impl/find_alt_impl.m
@@ -1,61 +1,82 @@
-function slsOuts_alt = find_alt_impl(sys, slsParams, slsOuts, Tc)
+function slsOuts_alt = find_alt_impl(sys, slsParams, slsOuts, Tc, approx)
 % Find alternate implementation, returned in slsOuts_alt
 % TODO: technically slsOuts_alt is a misnomer here as we didn't
 %       get it from SLS but from postprocessing SLS
 %    slsOuts_alt : contains clnorm and new R, M
 % Inputs
-%    sys         : LTISystem containing system matrices
-%    slsParams   : SLSParams containing parameters
-%    slsOuts     : contains info from SLS (original R, M)
+%    sys       : LTISystem containing system matrices
+%    slsParams : SLSParams containing parameters
+%    slsOuts   : contains info from SLS (original R, M)
+%    approx    : put any value here if you want approximate solution
+%                (i.e. drop trailing zero constraints)
+if nargin == 4
+    approx    = false;
+    statusTxt = 'Finding *exact* alt implementation';
+else
+    approx    = true;
+    statusTxt = 'Finding *approx* alt implementation';
+end
 
-statusTxt = sprintf('Finding alternate CL implementation with Tc=%d', Tc);
-statusTxt = [char(10), statusTxt];
+statusTxt = [char(10), statusTxt, sprintf(' with Tc=%d', Tc)];
 disp(statusTxt);
+
+T = slsParams.tFIR_;
 
 cvx_begin
 cvx_precision low
 
-TMax = max(Tc, slsParams.tFIR_);
-
 variable Rcs(sys.Nx, sys.Nx, Tc)
 variable Mcs(sys.Nu, sys.Nx, Tc)
-expression Dellcs(sys.Nx, sys.Nx, Tc)
-expression Rcsums(sys.Nx, sys.Nx, Tc)
-expression Mcsums(sys.Nu, sys.Nx, Tc)
+expression Dellcs(sys.Nx, sys.Nx, Tc+1)
+expression Rsums(sys.Nx, sys.Nx, Tc+T)
+expression Msums(sys.Nu, sys.Nx, Tc+T)
 
 % populate decision variables
 objective = 0;
 for t = 1:Tc
-    Rc{t}     = Rcs(:,:,t);
-    Mc{t}     = Mcs(:,:,t);
-    Dellc{t}  = Dellcs(:,:,t);
-    Rcsum{t}  = Rcsums(:,:,t);
-    Mcsum{t}  = Mcsums(:,:,t);
+    Rc{t}    = Rcs(:,:,t);
+    Mc{t}    = Mcs(:,:,t);
+    Dellc{t} = Dellcs(:,:,t);
+    Rsum{t}  = Rsums(:,:,t);
+    Msum{t}  = Msums(:,:,t);
     
     % L1 norm to enforce sparsity
     objective = objective + norm([sys.C1, sys.D12]*[Rc{t}; Mc{t}], 1);
 end
-
-% Not strictly necessary but makes everything easier
-% Equivalent to Rc{1} == eye(sys.Nx)
-% Should not reduce dimensionality of feasible Rc/Mc space
-Dellc{1} = eye(sys.Nx);
-
-for t=2:Tc-1
-    Dellc{t} = Rc{t+1} - sys.A*Rc{t} - sys.B2*Mc{t};
-end
-Dellc{Tc} = -sys.A*Rc{Tc} - sys.B2*Mc{Tc};
-for t=Tc+1:TMax
-    Dellc{t} = zeros(sys.Nx, sys.Nx);
+Dellc{Tc+1} = Dellcs(:,:,Tc+1);
+for t=Tc+1:Tc+T
+    Rsum{t}  = Rsums(:,:,t);
+    Msum{t}  = Msums(:,:,t);
 end
 
+% calculate Dellc
+Dellc{1} = eye(sys.Nx); % will enforce Rc{1} == eye(sys.Nx)
+for t=2:Tc
+    Dellc{t} = Rc{t} - sys.A*Rc{t-1} - sys.B2*Mc{t-1};
+end
+Dellc{Tc+1} = -sys.A*Rc{Tc} - sys.B2*Mc{Tc};
+for t=Tc+2:Tc+T
+    Dellc{t} = zeros(sys.Nx, sys.Nx); % zero padding for ease of calculation
+end
+
+% enforce LHS = RHS constraints
 for t=1:Tc
-    for k=1:min([slsParams.tFIR_, t]) % convolve
-         Rcsum{t} = Rcsum{t} + slsOuts.R_{k} * Dellc{t-k+1};
-         Mcsum{t} = Mcsum{t} + slsOuts.M_{k} * Dellc{t-k+1};
+    for k=1:min(T, t) % convolve
+         Rsum{t} = Rsum{t} + slsOuts.R_{k} * Dellc{t-k+1};
+         Msum{t} = Msum{t} + slsOuts.M_{k} * Dellc{t-k+1};
     end
-    Rc{t} == Rcsum{t};
-    Mc{t} == Mcsum{t};
+    Rc{t} == Rsum{t};
+    Mc{t} == Msum{t};
+end
+if not(approx) % do not included these for approxmation
+    for t=Tc+1:Tc+T
+        for k=1:min(T, t)
+             Rsum{t} = Rsum{t} + slsOuts.R_{k} * Dellc{t-k+1};
+             Msum{t} = Msum{t} + slsOuts.M_{k} * Dellc{t-k+1};
+        end
+        Rsum{t} == 0;
+        Msum{t} == 0;
+    end
 end
 
 minimize(objective);
