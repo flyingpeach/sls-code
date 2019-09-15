369763425178e33a2dd0a884ec87d6ce2e424673
Lisa Li <flyingpeach@live.com>
2019-09-14 22:56:19 -0700

    Allow plotting select Tcs
    This way we can scan a wide range of Tcs but plot
    more selectively depending on which are feasible

--- code/lisa/alt_impl/alt_impl_main.m
+++ code/lisa/alt_impl/alt_impl_main.m
@@ -1,5 +1,5 @@
 %% choose the system you want to work with
-setup1;
+setup3;
 
 % for rank/zero conditions, try to match the precision of cvx_precision low
 % http://cvxr.com/cvx/doc/solver.html#solver-precision
@@ -13,7 +13,7 @@ slsOuts_alt = find_alt_impl(sys, slsParams, slsOuts, Tc);
 
 s_a{1}  = slsOuts_alt;
 
-met     = AltImplMetrics([Tc], tol);
+met     = AltImplMetrics(Tc, tol);
 met     = calc_mtx_metrics(met, sys, slsParams, slsOuts, s_a);
 met     = calc_cl_metrics(met, sys, simParams, slsParams, slsOuts, s_a);
 met
@@ -25,7 +25,7 @@ visualize_matrices(slsOuts, slsOuts_alt, Tc, 'all');
 disp(['Statuses:', print_statuses(sys, s_a, rankF, rankF2)]); 
 
 %% find new implementations Rc, Mc
-Tcs    = [2:25];
+Tcs    = 2:slsParams.tFIR_+5;
 numTcs = length(Tcs);
 
 % slsOuts_alts{i} contains alternate implementation for Tc=Tcs(i)
@@ -43,15 +43,34 @@ end
 % check solver/feasibility statuses
 disp(['Statuses:', print_statuses(sys, slsOuts_alts, rankFs, rankF2s)]); 
 
-%% calculate stats & generate plots
-met     = AltImplMetrics(Tcs, tol);
+%% plot stuff
+% we might not want to plot all of slsOuts_alts, so this is the sandbox to
+% adjust slsOuts_alts / Tcs to our liking
+
+TcsWanted = 4:20;
+myIdx = [];
+
+for i=1:numTcs
+    Tc = Tcs(i);
+    if ismember(Tc, TcsWanted)
+        myIdx = [myIdx, i];
+    end
+end
+
+Tcs_plot = Tcs(myIdx);
+slsOuts_alts_plot = slsOuts_alts(myIdx);
+
+met = AltImplMetrics(Tcs_plot, tol);
 
 % calculate matrix-specific metrics
-met = calc_mtx_metrics(met, sys, slsParams, slsOuts, slsOuts_alts);
+met = calc_mtx_metrics(met, sys, slsParams, slsOuts, slsOuts_alts_plot);
 
 % calculate closed-loop metrics and plot select heat maps
-met = calc_cl_metrics(met, sys, simParams, slsParams, slsOuts, slsOuts_alts);
+met = calc_cl_metrics(met, sys, simParams, slsParams, slsOuts, slsOuts_alts_plot);
 
 % plot metrics and save to file
 savepath = 'C:\Users\Lisa\Desktop\caltech\research\implspace\tmp\';
 plot_metrics(met, savepath);
+
+% print again
+disp(['Statuses:', print_statuses(sys, slsOuts_alts, rankFs, rankF2s)]); 
