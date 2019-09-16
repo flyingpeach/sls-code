f3b409968374dc80304010b67bb7ba85d5666605
Lisa Li <flyingpeach@live.com>
2019-09-16 13:05:52 -0700

    Add robust formulation for approximate alt impl

--- code/lisa/alt_impl/tools/find_alt_impl_precise.m
+++ code/lisa/alt_impl/tools/find_alt_impl_precise.m
@@ -1,4 +1,4 @@
-function slsOuts_alt = find_alt_impl_precise(sys, slsParams, slsOuts, Tc)
+function slsOuts_alt = find_alt_impl_precise(sys, slsParams, slsOuts, Tc, approx, CLDiffPen)
 % Find alternate implementation, returned in slsOuts_alt
 % Rearranged [Rc; Mc] = [R; M] [zI-A -B] [Rc; Mc] into the form
 %   F2*[Rc; Mc] = F1 where F1, F2 do not depend on [Rc, Mc]
@@ -9,6 +9,17 @@ function slsOuts_alt = find_alt_impl_precise(sys, slsParams, slsOuts, Tc)
 %    slsParams   : SLSParams containing parameters
 %    slsOuts     : contains info from SLS (original R, M)
 %    Tc          : length of the approximate solution
+%    approx      : put any value here if you want approximate solution
+%    CLDiffPen   : used in approximation; penalize deviation of new CL map
+%                  from original CL map                   
+
+if nargin == 4
+    approx    = false;
+    statusTxt = 'Finding *exact* alt implementation';
+else
+    approx    = true;
+    statusTxt = 'Finding *approx* alt implementation';
+end
 
 statusTxt = 'Finding *exact* alt implementation';
 statusTxt = [char(10), statusTxt, sprintf(' with Tc=%d', Tc)];
@@ -20,6 +31,7 @@ cvx_precision low
 
 variable Rcs(sys.Nx * (Tc-1), sys.Nx) % Rc{1} == 1 so it's not a variable
 variable Mcs(sys.Nu * Tc, sys.Nx)
+expression Delta(Tc * (sys.Nx + sys.Nu) - sys.Nx, sys.Nx) 
 
 % helper function to convert cells of block matrices into giant matrix
 % copied from get_F
@@ -45,7 +57,13 @@ F  = get_F(sys, slsParams, slsOuts, Tc);
 F1 = F(:, 1:sys.Nx);
 F2 = F(:,sys.Nx+1:end);
 
-F2 * [Rcs; Mcs] == -F1;
+if approx
+    Delta = F2 * [Rcs; Mcs] + F1;    
+    CLDiff = norm(Delta);
+    objective = objective + CLDiffPen * CLDiff;
+else % exact solution
+    F2 * [Rcs; Mcs] == -F1;
+end
 
 minimize(objective);
 cvx_end
@@ -53,5 +71,10 @@ cvx_end
 % outputs
 slsOuts_alt.R_           = Rc;
 slsOuts_alt.M_           = Mc;
+
+% want original L1norm
+if approx
+    objective = objective - CLDiffPen * CLDiff;
+end
 slsOuts_alt.clnorm_      = objective;
 slsOuts_alt.solveStatus_ = cvx_status;
