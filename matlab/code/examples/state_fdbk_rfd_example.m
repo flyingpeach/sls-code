e1f42fe87642683e6e6a10a4f3028917a745a853
Lisa Li <flyingpeach@live.com>
2020-03-26 15:02:20 -0700

    Reduce number of examples for rfd

--- matlab/code/examples/state_fdbk_rfd_example.m
+++ matlab/code/examples/state_fdbk_rfd_example.m
@@ -19,31 +19,7 @@ slsParams      = SLSParams();
 slsParams.T_   = 15;
 slsParams.obj_ = Objective.H2; % objective function
 
-%% (1) basic sls (centralized controller) with rfd
-num_acts = []; clnorms = [];
-
-slsParams.mode_ = SLSMode.Basic;
-
-for power = -2:1:3
-    slsParams.rfdCoeff_ = 10^power;
-    slsParams.rfd_      = true;
-    slsOutsRFD1         = state_fdbk_sls(sys, slsParams);
-    
-    % check performance with rfd-designed system
-    sysAfterRFD1     = updateActuation(sys, slsOutsRFD1);
-    slsParams.rfd_   = false;
-    slsOutsAfterRFD1 = state_fdbk_sls(sysAfterRFD1, slsParams);
-
-    num_acts         = [num_acts; length(slsOutsRFD1.acts_)];
-    clnorms          = [clnorms; slsOutsAfterRFD1.clnorm_];
-end
-
-figure
-plot(num_acts,clnorms,'*-');
-xlabel('Number of actuators'); ylabel('Close loop norm');
-title('Centralized RFD tradeoff curve');
-
-%% (2) d-localized sls with rfd
+% delayed and localized sls with rfd
 num_acts = []; clnorms = [];
 
 slsParams.mode_      = SLSMode.DAndL;
@@ -69,29 +45,3 @@ figure;
 plot(num_acts,clnorms,'*-');
 xlabel('Number of actuators'); ylabel('Close loop norm');
 title('d-localized RFD tradeoff curve');
-
-%% (3) approximate d-localized sls with rfd
-num_acts = []; clnorms = [];
-
-slsParams.mode_      = SLSMode.DAndL;
-slsParams.approx_    = true;
-slsParams.robCoeff_  = 10^4;
-
-for power = -2:1:3
-    slsParams.rfdCoeff_ = 10^power;
-    slsParams.rfd_      = true;
-    slsOutsRFD3         = state_fdbk_sls(sys, slsParams);
-    
-    % check performance with rfd-designed system
-    sysAfterRFD3     = updateActuation(sys, slsOutsRFD3);
-    slsParams.rfd_   = false;
-    slsOutsAfterRFD3 = state_fdbk_sls(sysAfterRFD3, slsParams);
-
-    num_acts         = [num_acts; length(slsOutsRFD3.acts_)];
-    clnorms          = [clnorms; slsOutsAfterRFD3.clnorm_];
-end
-
-figure;
-plot(num_acts,clnorms,'*-');
-xlabel('Number of actuators'); ylabel('Close loop norm');
-title('Approx d-localized RFD tradeoff curve');
