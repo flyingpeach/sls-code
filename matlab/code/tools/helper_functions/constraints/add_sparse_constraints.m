c4369833ead909aa3f84cc78468472d6315af4be
Lisa Li <flyingpeach@live.com>
2020-03-26 16:09:15 -0700

    Pass T directly instead of params

--- matlab/code/tools/helper_functions/constraints/add_sparse_constraints.m
+++ matlab/code/tools/helper_functions/constraints/add_sparse_constraints.m
@@ -1,6 +1,6 @@
-function [R, M] = add_sparse_constraints(params, R, M, RSupp, MSupp, X)
+function [R, M] = add_sparse_constraints(R, M, RSupp, MSupp, X, T)
     spot = 0;
-    for t = 1:params.T_
+    for t = 1:T
         suppR = find(RSupp{t});
         num = sum(sum(RSupp{t}));
         R{t}(suppR) = X(spot+1:spot+num);
--- matlab/code/tools/state_fdbk_sls.m
+++ matlab/code/tools/state_fdbk_sls.m
@@ -1,31 +1,34 @@
 function clMaps = state_fdbk_sls(sys, params)
 % System level synthesis with state feedback
 % Returns 
-%    slsOuts: SLSOutputs containing system responses and other info
+%    clMaps : SLSOutputs containing system responses and other info
 % Inputs
 %    sys     : LTISystem containing system matrices
 %    params  : SLSParams containing parameters
+fprintf('Finding closed loop maps\n\n');
 params.print()
 params.sanity_check()
+
+T = params.T_;
 
 cvx_begin quiet
 
 if isempty(params.constraints_) % SLS with no constraints
-    variable Rs(sys.Nx, sys.Nx, params.T_)
-    variable Ms(sys.Nu, sys.Nx, params.T_)
+    variable Rs(sys.Nx, sys.Nx, T)
+    variable Ms(sys.Nu, sys.Nx, T)
 else 
-    expression Rs(sys.Nx, sys.Nx, params.T_)
-    expression Ms(sys.Nu, sys.Nx, params.T_)
+    expression Rs(sys.Nx, sys.Nx, T)
+    expression Ms(sys.Nu, sys.Nx, T)
 end
 
 if params.approx_
-    expression Delta(sys.Nx, sys.Nx * params.T_)
+    expression Delta(sys.Nx, sys.Nx * T)
 end
 
 % populate decision variables for ease-of-use
-R = cell(params.T_, 1); 
-M = cell(params.T_, 1);
-for t = 1:params.T_
+R = cell(T, 1); 
+M = cell(T, 1);
+for t = 1:T
     R{t} = Rs(:,:,t); M{t} = Ms(:,:,t);
 end
 
@@ -33,21 +36,21 @@ end
 if ~isempty(params.constraints_)
     [RSupp, MSupp, count] = get_supports(sys, params);
     variable RMSuppVals(count)
-    [R, M] = add_sparse_constraints(params, R, M, RSupp, MSupp, RMSuppVals);
+    [R, M] = add_sparse_constraints(R, M, RSupp, MSupp, RMSuppVals, T);
 end
 objective = get_total_objective(sys, params, R, M);
 
 % achievability  / approx achievability constraints
 R{1} == eye(sys.Nx);
-R{params.T_} == zeros(sys.Nx, sys.Nx);
+R{T} == zeros(sys.Nx, sys.Nx);
 
 if params.approx_
-    for t=1:params.T_-1
+    for t=1:T-1
         Delta(:,(t-1)*sys.Nx+1:t*sys.Nx) = R{t+1} - sys.A*R{t} - sys.B2*M{t};
     end
     objective = objective + params.approxCoeff_ * norm(Delta, inf);    
 else
-    for t=1:params.T_-1
+    for t=1:T-1
         R{t+1} == sys.A*R{t} + sys.B2*M{t};
     end
 end
