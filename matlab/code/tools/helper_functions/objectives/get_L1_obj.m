477f9293df9be526023815b61d9ac82428713207
Lisa Li <flyingpeach@live.com>
2020-03-27 16:34:31 -0700

    Fix typo in L1 objective

--- matlab/code/tools/helper_functions/objectives/get_L1_obj.m
+++ matlab/code/tools/helper_functions/objectives/get_L1_obj.m
@@ -1,6 +1,6 @@
 function objective = get_L1_obj(transferMtx)
 objective = 0;
 for k=1:length(transferMtx)
-    objective = max(objective, norm(transferMtx{k}));
+    objective = max(objective, norm(transferMtx{k}, Inf));
 end
 end
