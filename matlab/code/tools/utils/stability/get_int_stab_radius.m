8f67d8518bc6835cc3138358a8cdd0fcf910a225
Lisa Li <flyingpeach@live.com>
2020-03-30 20:18:54 -0700

    Rename function

--- matlab/code/tools/utils/stability/get_int_stab_radius.m
+++ matlab/code/tools/utils/stability/get_int_stab_radius.m
@@ -1,4 +1,4 @@
-function spectralRadius = check_int_stab(sys, ctrller)
+function spectralRadius = get_int_stab_radius(sys, ctrller)
 
 intStabMtx     = get_int_stab_mtx(sys, ctrller);
 spectralRadius = max(abs(eig(intStabMtx))); % biggest eigenvalue
