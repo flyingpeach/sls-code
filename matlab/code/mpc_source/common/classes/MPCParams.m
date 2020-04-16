b379a0c32d87b720ee9b1defc14c37e80322fc12
Lisa Li <flyingpeach@live.com>
2020-04-16 13:23:32 -0700

    Add sanity checks for mpc params

--- matlab/code/mpc_source/common/classes/MPCParams.m
+++ matlab/code/mpc_source/common/classes/MPCParams.m
@@ -23,4 +23,34 @@ classdef MPCParams < matlab.mixin.Copyable
         eps_x_; % convergence criterion for ||X(n+1) - Z(n+1)||
         eps_z_; % convergence criterion for ||Z(n+1) - Z(n)||
     end
+    
+    methods
+      function sanity_check_alg_1(obj)
+          e1 = isempty(obj.d_);
+          e2 = isempty(obj.tFIR_);
+          e3 = isempty(obj.tHorizon_);
+          e4 = isempty(obj.maxIters_);
+          e5 = isempty(obj.rho_);
+          e6 = isempty(obj.eps_p_);
+          e7 = isempty(obj.eps_d_);
+          
+          if (e1 || e2 || e3 || e4 || e5 || e6 || e7)
+              sls_error('One or more required parameters is missing!')
+          end
+      end
+      
+      function sanity_check_alg_2(obj)
+          sanity_check_alg_1(obj);
+          
+          e1 = isempty(obj.maxItersCons_);
+          e2 = isempty(obj.mu_);
+          e3 = isempty(obj.eps_x_);
+          e4 = isempty(obj.eps_z_);
+          
+          if (e1 || e2 || e3 || e4)
+              sls_error('One or more required parameters is missing!')
+          end
+          
+      end
+    end
 end
