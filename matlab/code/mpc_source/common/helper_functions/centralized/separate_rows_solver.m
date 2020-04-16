ae1b7d218e16aa4e4a13ee80e8f2cc9555dfd0b3
Lisa Li <flyingpeach@live.com>
2020-04-16 16:09:31 -0700

    Introduce new function for separating rows with gurobi

---
+++ matlab/code/mpc_source/common/helper_functions/centralized/separate_rows_solver.m
@@ -0,0 +1,28 @@
+function [Psi_rows, Lambda_rows] = separate_rows_solver(sys, tFIR, ...
+                                                        r, s_r, r_loc, m_loc, ...
+                                                        Psi, Lambda)
+Nx = sys.Nx; Nu = sys.Nu;
+
+Psi_rows    = cell(1, Nx);
+Lambda_rows = cell(1, Nx);
+                
+% Separate the given matrices
+k = 0;
+for i_ = 1:Nx
+    if mod(i_, Nx/Nu) == 0
+        k = k+1;
+    end
+    
+    for i = r{i_}
+        j = find(r{i_}==i);
+        if j<=tFIR
+            Psi_rows{i} = Psi(i,s_r{i_}(j,1:max(length(find(r_loc{j}(i_,:))))));
+            Lambda_rows{i} = Lambda(i,s_r{i_}(j,1:max(length(find(r_loc{j}(i_,:))))));
+        else
+            Psi_rows{i} = Psi(i,s_r{i_}(j,1:max(length(find(m_loc{j-tFIR}(k,:))))));
+            Lambda_rows{i} = Lambda(i,s_r{i_}(j,1:max(length(find(m_loc{j-tFIR}(k,:))))));
+        end
+    end
+end
+
+end
