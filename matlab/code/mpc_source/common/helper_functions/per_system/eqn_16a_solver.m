b6409c0fcc72ae7d0b2b1ce2592945c812a43964
Lisa Li <flyingpeach@live.com>
2020-04-20 16:26:53 -0700

    Clarify gurobi code

--- matlab/code/mpc_source/common/helper_functions/per_system/eqn_16a_solver.m
+++ matlab/code/mpc_source/common/helper_functions/per_system/eqn_16a_solver.m
@@ -2,16 +2,25 @@ function [Phi_loc, time] = eqn_16a_solver(x_ri, psi_rowi, lamb_rowi, n, mParams)
 
     rho = mParams.rho_;
     
+    % set up QP
+    % minimize Phi*Q*Phi' + obj*Phi'
+    % note: constant terms omitted since we are interested in argmin only
     model.Q     = sparse(x_ri*x_ri' + rho/2*eye(n));
     model.obj   = rho*(-psi_rowi + lamb_rowi);
+    
+    % s.t. x_ri'*Phi < upperbnd
+    %      x_ri'*Phi > lowerbnd
     model.A     = sparse([x_ri'; x_ri']);
     model.rhs   = [mParams.state_upperbnd_, mParams.state_lowerbnd_];
-    model.sense = '<>';   
-    model.lb    = -100*ones(n, 1); % TODO: hacky?
+    model.sense = '<>';
     
+    % default lower bound is 0; override
+    MPC_LB   = -100;
+    model.lb = MPC_LB*ones(n, 1);
+    
+    % solve QP    
     gParams.outputflag = 0;
-    result = gurobi(model, gParams);
-    time   = result.runtime;
-    
+    result  = gurobi(model, gParams);
+    time    = result.runtime;
     Phi_loc = result.x(:);
 end
