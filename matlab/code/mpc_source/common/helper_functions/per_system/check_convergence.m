56b6907496cef06172501abcdf09cc61cc11e0b5
Lisa Li <flyingpeach@live.com>
2020-04-21 18:37:27 -0700

    Change naming convention in convergence function

--- matlab/code/mpc_source/common/helper_functions/per_system/check_convergence.m
+++ matlab/code/mpc_source/common/helper_functions/per_system/check_convergence.m
@@ -1,7 +1,7 @@
 function converged = check_convergence(phi_loc, psi_loc, psi_prev_loc, eps_p, eps_d)
     
-    primDiff  = norm(phi_loc-psi_loc,'fro');
-    dualDiff  = norm(psi_loc-psi_prev_loc,'fro');
-    converged = primDiff <= eps_p && dualDiff <= eps_d;
+    primRes   = norm(phi_loc-psi_loc,'fro');
+    dualRes   = norm(psi_loc-psi_prev_loc,'fro');
+    converged = primRes <= eps_p && dualRes <= eps_d;
 
 end
