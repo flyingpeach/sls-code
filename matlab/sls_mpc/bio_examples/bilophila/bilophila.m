fcec6489d52bab560a0652992e55301924f98f81
Lisa Li <flyingpeach@live.com>
2020-05-05 00:28:57 -0700

    Initial try of mpc_bilo [not verified]

--- matlab/sls_mpc/bio_examples/bilophila/bilophila.m
+++ matlab/sls_mpc/bio_examples/bilophila/bilophila.m
@@ -1,3 +1,5 @@
+clear all; close all; clc;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % User-defined
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -5,11 +7,13 @@
 q = 0.8;
 
 % initial conditions
-x0 = 0.1 * ones(Nx, 1);
-u0 = zeros(Nu, 1);
+x0 = 0.1*ones(5, 1);
 
 % simulation length
-tHorizon = 20;
+tHorizon = 10;
+
+% SLS length
+tFIR = 5;
 
 % disturbance
 ws = [0.1 * ones(1, tHorizon/2), ones(1, tHorizon/2)];
@@ -23,36 +27,52 @@ u_lb  = [-1; -1; -1];
 u_ub  = [0; 1; 0];
 x4_lb = 1e-3;
     
-Nx       = 5;
-Nu       = 3;
+Nx = 5;
+Nu = 3;
 
-xs         = zeros(Nx, tHorizon);
-us         = zeros(Nu, tHorizon);
-xs(:,1) = x0; us(:,1) = u0;
+sys = LTISystem();
+sys.Nx = Nx; sys.Nu = Nu;
+
+xs      = zeros(Nx, tHorizon);
+us      = zeros(Nu, tHorizon);
+xs(:,1) = x0;
+
+e1 = [1 0 0 0 0]';
 
 for t=1:tHorizon-1
+    fprintf('Time: %d\n', t);
+    
     x_ = xs(:,t);
     u_ = us(:,t); % always zero
     w_ = ws(:,t);
 
-    [Ac, Bc] = linearize_bilo(x_, u_, q);
-    [A, B]   = discretize(Ac, Bc, Ts);
+    [Ac, Bc]        = linearize_bilo(x_, u_, q);    
+    [sys.A, sys.B2] = discretize(Ac, Bc, Ts);
     
     % coordinate shift bounds
-    u_tilde_lb  = u_lb - u_*ones(Nu, 1);
-    u_tilde_ub  = u_ub - u_*ones(Nu, 1);
-    x4_tilde_lb = x4_lb - x_;
+    u_tilde_lb  = u_lb - u_;
+    u_tilde_ub  = u_ub - u_;
+    x4_tilde_lb = x4_lb - x_(4);
     
-    % do MPC with (A,B), x0=0 (since we're in coordinate-shifted regime)
-    % remember to include x_(5) in objective    
-    % MPC returns y (coordinate-shifted x), u_tilde 
+    % y, u_tilde are coordinate shifts of x, u
+    y0 = pinv(sys.A) * f_bilo(x_, u_, w_, q);
     
+    [y, u_tilde] = mpc_bilo(sys, tFIR, u_tilde_lb, u_tilde_ub, x4_tilde_lb, y0);
+
     % calculate x(t+1) from dynamics and u_tilde directly
-    x_tilde_nxt = f_bilo(x_, u_, w_, q)*Ts + B*u_tilde;
-    xs(:,t+1) = x_ + x_tilde_next;
+    x_tilde_nxt = f_bilo(x_, u_, w_, q)*Ts + sys.B2*u_tilde + e1*(ws(:,t+1) - w_);
+    xs(:,t+1) = x_ + x_tilde_nxt;
     
     % update actuation
     us(:,t) = u_ + u_tilde;    
 end
 
+%% Plot
+figure(); 
+hold on;
 
+for i=1:5
+    plot(1:tHorizon, xs(i,:));
+end
+
+legend('x1 (taurine)', 'x2 (sulfite)', 'x3 (sulfide)', 'x4 (energy)', 'x5 (growth)'); 
---
+++ matlab/sls_mpc/bio_examples/bilophila/mpc_bilo.m
@@ -0,0 +1,63 @@
+function [x, u] = mpc_bilo(sys, tFIR, u_lb, u_ub, x4_lb, xt)
+% Note that these are "x, u" in the local sense of mpc
+% In the bilophila example, they are y and u_tilde (shifted coordinates)
+
+Nx = sys.Nx; Nu = sys.Nu; A = sys.A; B = sys.B2;
+
+RLoc = ones(Nx, Nx); % no sparsity constraint
+MLoc = [1 0 0 1 0;
+        0 0 0 1 0;
+        0 0 0 1 0];
+
+count = 0;
+for k = 1:tFIR
+    RSupp{k} = RLoc;
+    MSupp{k} = MLoc;
+    count = count + sum(sum(RSupp{k})) + sum(sum(MSupp{k}));
+end
+
+cvx_begin
+
+variable X(count)
+expression Rs(Nx, Nx, tFIR)
+expression Ms(Nu, Nx, tFIR)
+   
+R = cell(tFIR, 1);
+M = cell(tFIR, 1);
+
+for t = 1:tFIR
+    R{t} = Rs(:,:,t); M{t} = Ms(:,:,t); 
+end
+
+[R, M] = add_sparse_constraints(R, M, RSupp, MSupp, X, tFIR);
+
+% maximizing x(5) is the same as maximizing x_tilde(5)
+e5 = [0 0 0 0 1];
+
+objective = 0;
+for k = 1:tFIR
+    objective = objective + e5*R{k}*xt;
+end
+
+maximize(objective)
+subject to
+
+% Achievability constraints
+R{1} == eye(Nx); 
+for k=1:tFIR-1
+    R{k+1} == A*R{k} + B*M{k};
+end
+
+e4 = [0 0 0 1 0];
+
+for k=1:tFIR
+    M{k}*xt >= u_lb;
+    M{k}*xt <= u_ub;
+    e4*R{k}*xt >= x4_lb;
+end
+cvx_end
+
+u = M{1}*xt;
+x = R{2}*xt;
+
+end
