41ece065ec638ebaadc1b5381624d65d393ef110
Lisa Li <flyingpeach@live.com>
2020-05-04 22:59:41 -0700

    Add coordinate shift for x_tilde

--- matlab/sls_mpc/bio_examples/bilophila/bilophila.m
+++ matlab/sls_mpc/bio_examples/bilophila/bilophila.m
@@ -11,11 +11,8 @@ u0 = zeros(Nu, 1);
 % simulation length
 tHorizon = 20;
 
-% expected "disturbance" (MPC knows this and designs for it)
-w_expect = 0.1 * ones(1, tHorizon);
-
-% unknown "disturbance" (step)
-w_actual = [0.1 * ones(1, tHorizon/2), ones(1, tHorizon/2)];
+% disturbance
+ws = [0.1 * ones(1, tHorizon/2), ones(1, tHorizon/2)];
 
 % sampling time for discretization
 Ts = 0.1;
@@ -25,8 +22,6 @@ Ts = 0.1;
 u_lb  = [-1; -1; -1];
 u_ub  = [0; 1; 0];
 x4_lb = 1e-3;
-
-D = [1 0 0 0 0]'; % x_dot = f(x, u) + D*w
     
 Nx       = 5;
 Nu       = 3;
@@ -37,8 +32,8 @@ xs(:,1) = x0; us(:,1) = u0;
 
 for t=1:tHorizon-1
     x_ = xs(:,t);
-    u_ = us(:,t);
-    w_ = w_expect(:,t);
+    u_ = us(:,t); % always zero
+    w_ = ws(:,t);
 
     [Ac, Bc] = linearize_bilo(x_, u_, q);
     [A, B]   = discretize(Ac, Bc, Ts);
@@ -46,10 +41,15 @@ for t=1:tHorizon-1
     % coordinate shift bounds and pass in to MPC
     
     % do MPC with (A,B), x0=0 (since we're in coordinate-shifted regime)
-    % remember to include x_(5) in objective
+    % remember to include x_(5) in objective    
+    % MPC returns y (coordinate-shifted x), u_tilde 
     
-    % MPC returns y (coordinate-shifted x), u_tilde
-    % calculate x from dynamics and u_tilde directly    
+    % calculate x(t+1) from dynamics and u_tilde directly
+    x_tilde_nxt = f_bilo(x_, u_, w_, q)*Ts + B*u_tilde;
+    xs(:,t+1) = x_ + x_tilde_next;
+    
+    % update actuation
+    us(:,t) = u_ + u_tilde;    
 end
 
 
