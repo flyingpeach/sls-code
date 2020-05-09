6d49b438fefbb5ff81c40ebb13a38724599e52c4
Lisa Li <flyingpeach@live.com>
2020-05-08 19:33:22 -0700

    Clean up fba code

--- matlab/sls_mpc/bio_examples/glycolysis/fba_glyco.m
+++ matlab/sls_mpc/bio_examples/glycolysis/fba_glyco.m
@@ -1,56 +1,65 @@
-%% Flux Balance Analysis
 clear all; clc
-%% Parameters
 
-Nx = 3;
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% User-defined
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % model parameters
 q = 0.8;
 k = [2 4 0.1];
-c = [0 0 -1]';
 
 % initial conditions
-xs(:,1) = [0.6; 0.6; 0.1];
+x0 = [0.6; 0.6; 0.1];
 
 % simulation length
-tHorizon = 10;
+tHorizon = 5;
 
 % disturbance (step)
 val1 = 0;  % value of first disturbance
-dur1 = 5;  % how long first disturbance lasts
+dur1 = 2;  % how long first disturbance lasts
 val2 = -3; % value of second disturbance (lasts forever)
 ws = [val1*ones(1, dur1) val2*ones(1, tHorizon-dur1)];
-
-% stoichiometry matrix
-S = [1 -1 0; -q q+1 -1; 0 0 1];
 
 % sampling time for discretization
 Ts = 0.1;
 
-%% LP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Setup
+% stoichiometry matrix
+Nx = 3;
+S = [ 1   -1  0;
+     -q  q+1 -1
+      0    0  1];
+
+K = diag(k);
+
+xs      = zeros(Nx, tHorizon);
+fluxes  = zeros(Nx, tHorizon);
+xs(:,1) = x0;
+
+e2 = [0 1 0]';
 
 for t = 1:tHorizon-1
 
     cvx_begin
 
     variable v(Nx,1)
+    maximize([0 0 1] * v);
+    subject to
+    S*K*v + e2*ws(t) == 0;
 
-    minimize (c'*v)
-    S*[k(1)*v(1);k(2)*v(2);k(3)*v(3)] == -[0 1 0]'*ws(t);
-    zeros(Nx,1) <= v;
-    v <= exp(ones(Nx,1));
+    v >= zeros(Nx, 1);
+    v <= exp(ones(Nx, 1));
 
     cvx_end
     
-    flux = [k(1)*v(1);k(2)*v(2);k(3)*v(3)];
+    flux = K*v;
     
-    %% Compute the state
-
-    deltax = (S*flux+[0 1 0]'*ws(t+1))*Ts;
-    xs(:,t+1) = xs(:,t)+deltax;
+    % update state
+    deltax    = (S*flux + e2*ws(t+1)) * Ts;
+    xs(:,t+1) = xs(:,t) + deltax;
     
 end
-%%
+%% Plot
 figure();
 
 subplot(2,1,1);
