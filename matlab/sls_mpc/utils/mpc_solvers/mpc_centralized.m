5959b6fd23a6cd24cbd9b61437d5a7eb479623e6
Lisa Li <flyingpeach@live.com>
2020-05-22 18:42:37 -0700

    Add input constraints to centralized MPC

--- matlab/sls_mpc/utils/mpc_solvers/mpc_centralized.m
+++ matlab/sls_mpc/utils/mpc_solvers/mpc_centralized.m
@@ -64,7 +64,7 @@ for t = 1:tHorizon
     end
     
     if params.has_state_cons()
-        for k=2:tFIR % since we only care about later states
+        for k=2:tFIR % Can't change current state (k=1) so don't worry about it
             if ~isinf(params.stateUB_)
                 params.stateConsMtx_*R{k}*x_t <= params.stateUB_*ones(Nx, 1);
             end
@@ -73,6 +73,19 @@ for t = 1:tHorizon
                 params.stateConsMtx_*R{k}*x_t >= params.stateLB_*ones(Nx, 1);
             end
         end
+    end
+    
+    if params.has_input_cons()
+        for k=1:tFIR
+            if ~isinf(params.inputUB_)
+                params.inputConsMtx_*M{k}*x_t <= params.stateUB_*ones(Nu, 1);
+            end
+            
+            if ~isinf(params.inputLB_)
+                params.inputConsMtx_*M{k}*x_t >= params.stateLB_*ones(Nu, 1);
+            end
+        end
+        
     end
 
     cvx_end
