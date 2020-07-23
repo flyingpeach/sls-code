90fba74e424b75b0039a28ed302ce6c2650986c2
Lisa Li <flyingpeach@live.com>
2020-07-23 14:20:21 -0700

    Fix L1 norm definition

--- matlab/sls_base/utils/objectives/get_L1_norm.m
+++ matlab/sls_base/utils/objectives/get_L1_norm.m
@@ -1,7 +1,12 @@
 function objective = get_L1_norm(transferMtx)
 % Induced inf-to-inf objective (i.e. max row sum)
+
 objective = 0;
+
+rowConcat = [];
 for k=1:length(transferMtx)
-    objective = max(objective, norm(transferMtx{k}, Inf));
+    rowConcat = [rowConcat transferMtx{k}];
 end
+
+objective = norm(rowConcat, Inf);
 end
