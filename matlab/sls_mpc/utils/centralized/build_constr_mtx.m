4a047834cab35368266ffe4fe1b59441825db944
camoalon <camoalon@caltech.edu>
2021-02-09 22:04:31 -0800

    removed constraint from initial condition

--- matlab/sls_mpc/utils/centralized/build_constr_mtx.m
+++ matlab/sls_mpc/utils/centralized/build_constr_mtx.m
@@ -13,7 +13,8 @@ if params.has_input_cons()
     K2 = params.inputConsMtx_;
 end
     
-for t = 1:tFIR
+ConstrMtx = zeros(sys.Nx);
+for t = 2:tFIR
     ConstrMtx = blkdiag(ConstrMtx, K1);
 end
 for t = 1:tFIR-1
