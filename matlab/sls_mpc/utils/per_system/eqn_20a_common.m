2ad64764d64b5a34cbc4d7733b73855b74e9948b
Lisa Li <flyingpeach@live.com>
2021-04-29 12:58:18 -0700

    Add clarifying comments for eqn_20a

--- matlab/sls_mpc/utils/per_system/eqn_20a_common.m
+++ matlab/sls_mpc/utils/per_system/eqn_20a_common.m
@@ -3,8 +3,8 @@ function [M1, M2, MSum, MbSum] = eqn_20a_common(x_loc, y_, z_, selfIdx)
 n   = length(x_loc);
 nc  = length(z_);    % # of coupled subsystems (including self)
 
-MSum  = 0; 
-MbSum = 0;
+MSum  = 0; % "Quadratic" terms for the mu/2 objective term
+MbSum = 0; % "Linear" terms for the mu/2 objective term
 for j = 1:nc
     if j < selfIdx
             Mj      = zeros(1,nc+n-1); 
@@ -22,6 +22,7 @@ for j = 1:nc
     MbSum = MbSum + Mj'*b;
 end
 
+% These terms are not related to the mu/2 objective terms
 myEye = eye(nc-1);
 M1    = [eye(n) zeros(n,nc-1)];
 M2    = [zeros(selfIdx-1,n)  myEye(1:selfIdx-1,:); 
