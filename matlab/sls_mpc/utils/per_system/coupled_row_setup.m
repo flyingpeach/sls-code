648b3a8e305dc981d6dab79aee77df7ef8a39ef3
Lisa Li <flyingpeach@live.com>
2021-05-15 15:07:25 -0700

    Variable name change

--- matlab/sls_mpc/utils/per_system/coupled_row_setup.m
+++ matlab/sls_mpc/utils/per_system/coupled_row_setup.m
@@ -1,4 +1,4 @@
-function [M1, M2, MSum, MbSum] = coupled_row_setup(x_loc, y, z, selfIdx)
+function [M1, M2, MSum, MbSum] = coupled_row_setup(x_loc, y, z, sIdx)
 
 n   = length(x_loc);
 nc  = length(z); % # of coupled subsystems (including self)
@@ -6,12 +6,12 @@ nc  = length(z); % # of coupled subsystems (including self)
 MSum  = 0; % "Quadratic" terms for the mu/2 objective term
 MbSum = 0; % "Linear" terms for the mu/2 objective term
 for j = 1:nc
-    if j < selfIdx
+    if j < sIdx
             Mj      = zeros(1,nc+n-1); 
             Mj(n+j) = 1;
-    elseif j == selfIdx
+    elseif j == sIdx
             Mj      = [x_loc' zeros(1,nc-1)]; % Implicitly enforce Xi = Phi*x0
-    elseif j > selfIdx
+    elseif j > sIdx
             Mj        = zeros(1,nc+n-1); 
             Mj(n+j-1) = 1;
     end
@@ -24,8 +24,8 @@ end
 % These terms are not related to the mu/2 objective terms
 myEye = eye(nc-1);
 M1    = [eye(n) zeros(n,nc-1)];
-M2    = [zeros(selfIdx-1,n)  myEye(1:selfIdx-1,:); 
+M2    = [zeros(sIdx-1,n)  myEye(1:sIdx-1,:); 
          x_loc'            zeros(1,nc-1); 
-         zeros(nc-selfIdx,n) myEye(selfIdx:end,:)];                                 
+         zeros(nc-sIdx,n) myEye(sIdx:end,:)];                                 
                                  
 end
