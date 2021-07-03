87d8bc79134276371cf7dad587bb081dad0f9bf1
Lisa Li <flyingpeach@live.com>
2021-07-03 12:39:19 -0700

    Let sls_mpc use disturbance

--- matlab/sls_mpc/sls_mpc.m
+++ matlab/sls_mpc/sls_mpc.m
@@ -1,14 +1,15 @@
-function [xs, us, avgStats] = sls_mpc(sys, x0, params, tHorizon)
+ function [xs, us, avgStats] = sls_mpc(sys, x0, w, params, tHorizon)
 % Call this function once to run mpc on a non-time-varying system
 % Wrapper function for mpc_distributed and mpc_centralized, which are 
 % per-timestep functions
 % Inputs
 %   sys     : LTISystem containing system matrices (A, B2) and Nx, Nu 
 %   x0      : Starting system state
+%   w       : Disturbance
 %   params  : MPCParams containing parameters for mpc
-%   tHorizon: total time to run MPC for
+%   tHorizon: Total time to run MPC for
 % Outputs
-%   avgStats: average runtime, iters, consIters (per inner loop iter) per timestep
+%   avgStats: Average runtime, iters, consIters (per inner loop iter) per timestep
 %             iters and consIters will be empty for centralized MPC
 
 xs      = zeros(sys.Nx, tHorizon);
@@ -27,9 +28,9 @@ for t=1:tHorizon-1
     
     if params.mode_ == MPCMode.Distributed
         if params.accounts_for_disturbance() % robust MPC
-            [xs(:,t+1), us(:,t), stats, warmStart] = rmpc_distributed(sys, xs(:,t), params, warmStart);
+            [~, us(:,t), stats, warmStart] = rmpc_distributed(sys, xs(:,t), params, warmStart);
         else % noiseless MPC
-            [xs(:,t+1), us(:,t), stats, warmStart] = mpc_distributed(sys, xs(:,t), params, warmStart);
+            [~, us(:,t), stats, warmStart] = mpc_distributed(sys, xs(:,t), params, warmStart);
         end        
         times(t)     = stats.time_;
         iters(t)     = stats.iters_;
@@ -37,15 +38,17 @@ for t=1:tHorizon-1
         
     elseif params.mode_ == MPCMode.Centralized % centralized algorithms return no iteration info
         if params.accounts_for_disturbance() % robust MPC
-            [xs(:,t+1), us(:,t), times(t)] = rmpc_centralized(sys, xs(:,t), params);
+            [~, us(:,t), times(t)] = rmpc_centralized(sys, xs(:,t), params);
         else % standard MPC
-            [xs(:,t+1), us(:,t), times(t)] = mpc_centralized(sys, xs(:,t), params);
+            [~, us(:,t), times(t)] = mpc_centralized(sys, xs(:,t), params);
         end
-        
     else
         mpc_error('Unrecognized MPC mode specified!');
     end
-            
+    
+    % Ignore calculated MPC state since it doesn't take disturbance
+    % into account
+    xs(:,t+1) = sys.A*xs(:,t) + sys.B2*us(:,t) + sys.B1*w(:,t);
 end
 
 avgStats       = MPCStats();
