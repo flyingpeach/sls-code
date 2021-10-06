19db37c3a79a6be749c97eb2177c249017428808
Lisa Li <flyingpeach@live.com>
2021-10-06 11:12:01 -0700

    Make assign_rows_h one big function

--- matlab/sls_mpc/utils/centralized/cols_and_rows/assign_rows_h.m
+++ matlab/sls_mpc/utils/centralized/cols_and_rows/assign_rows_h.m
@@ -1,4 +1,15 @@
 function r = assign_rows_h(sys, params)
+
+if params.hasTerminalSet()
+    r = assign_rows_h_with_terminal_set(sys, params);
+else
+    r = assign_rows_h_no_terminal_set(sys, params);
+end
+end
+
+
+% Helper functions
+function r = assign_rows_h_no_terminal_set(sys, params)
 % r{i} represents the set of rows subsystem i solves for
 
 Nx = sys.Nx; Nu = sys.Nu; T = params.tFIR_;
@@ -70,6 +81,45 @@ for i = 1:Nx
                 r{i}(end+1) = rowOffset + nInputsLB*(t-1) + find(inputsLB == actuator);
             end
         end
+    end
+end
+
+end
+
+
+function r = assign_rows_h_with_terminal_set(sys, params)
+% r{i} represents the set of rows subsystem i solves for
+% Also contains sanity check for locality for terminal set 
+
+r = cell(Nx, 1);
+% Assign rows of original H matrix (minus state constraint at time T)
+% assuming UB/LB on all states, and no input bounds
+for i=1:Nx
+    for t=1:T-1
+        r{i}(end+1) = Nx*(t-1) + i;
+    end
+end
+
+HT         = params.terminal_H_;
+Comms_Adj  = abs(sys.A) > 0;
+StateSupp  = Comms_Adj^(params.locality_-1) > 0;
+
+for rowHT=1:size(HT, 1)    
+    Comms_Hi    = HT(rowHT, :) ~= 0; % boolean
+    locality_ok = false;
+    rowH        = rowHT + 2*Nx*(T-1);
+    
+    for i=find(HT(rowHT, :))
+        if isempty(find(StateSupp(i, :) - Comms_Hi < 0, 1))
+            % H(i,:) obeys locality sparsity for some node
+            % We can say H(i,:) "belongs" to this node
+            r{i}(end+1) = rowH;
+            locality_ok = true;
+        end
+    end
+    
+    if locality_ok == false
+        mpc_error('Terminal set coupling violates locality!');
     end
 end
 
