d48c03e2157e27d36d8fa27227bf66de53b18a83
Lisa Li <flyingpeach@live.com>
2021-11-29 15:58:02 -0800

    Remove unneeded consensus code from old formulation

--- matlab/sls_mpc/utils/per_system/consensus/check_convergence_cons.m
+++
@@ -1,7 +0,0 @@
-function converged = check_convergence_cons(z_cp, x, z, z_prev, params)
-
-primRes   = norm(x - z_cp, 'fro');
-dualRes   = norm(z - z_prev, 'fro');
-converged = primRes <= params.eps_x_ && dualRes <= params.eps_z_;
-
-end
